4d76230464bc26b408fc3b1ec45f6c39fbc24f1d
Kalhale Hanumant Angad <khanumant2023@gmail.com>
2025-01-08 16:58:55 +0530

    responsice

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,9 +3,9 @@ import { Link } from "react-router-dom"
 const Navbar = () => {
   return (
     <div>
-        <nav className='flex flex-row items-center justify-around'>
+        <nav  className='nav flex flex-row items-center justify-around'>
         <a href="#Home" className="text-zinc-800 no-underline"><h4 className='text-xl font-bold'>Hanumant </h4></a>
-            <div className='flex justify-around gap-4 '>
+            <div className='nav flex justify-around gap-4 '>
                 <a className='no-underline text-zinc-800 hover:text-zinc-950 hover:font-semibold' href="#Home" >Home</a>
                 <a className='no-underline text-zinc-800 hover:text-zinc-950 hover:font-semibold' href="#Skills" >Skills</a>
                 <a className='no-underline text-zinc-800 hover:text-zinc-950 hover:font-semibold' href="#About" >About</a>
