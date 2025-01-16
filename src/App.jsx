2a3b0c7416c7c0b4eb281fd1a9acf7a495945447
Kalhale Hanumant Angad <khanumant2023@gmail.com>
2025-01-16 16:02:12 +0530

    second commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import Navbar from "./Components/Navbar"
+import Navbar from './components/Navbar'
 import Home from "./components/Home"
 import About from "./components/About"
 import Skills from "./components/Skills"
